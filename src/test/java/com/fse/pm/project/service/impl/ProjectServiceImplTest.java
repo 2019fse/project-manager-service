afb3272de48b8bcd5727ac8d8cd8fdf620ded9aa
selvak <sonytv710@gmail.com>
2019-03-13 23:38:26 -0400

    Add test cases

--- src/test/java/com/fse/pm/project/service/impl/ProjectServiceImplTest.java
+++ src/test/java/com/fse/pm/project/service/impl/ProjectServiceImplTest.java
@@ -91,4 +91,44 @@ public class ProjectServiceImplTest {
         verify(userRepository, times(1)).findUserByProjectId(any());
         verify(projectRepository, times(1)).deleteById(any());
     }
+
+    @Test
+    public void deleteProjectNullUser() {
+        when(userRepository.findUserByProjectId(anyInt())).thenReturn(null);
+        doNothing().when(projectRepository).deleteById(anyInt());
+        projectService.deleteProject(1);
+        verify(userRepository, times(1)).findUserByProjectId(any());
+        verify(projectRepository, times(1)).deleteById(any());
+    }
+
+    @Test
+    public void updateProjectNullUser() {
+        ProjectRequest projectRequest = TestUtil.getTestProjectRequest();
+        projectRequest.setManagerId(null);
+        when(projectRepository.save(any())).thenReturn(TestUtil.getTestProject());
+        when(userRepository.save(any())).thenReturn(TestUtil.getTestUser());
+        when(userRepository.findUserByProjectId(anyInt())).thenReturn(TestUtil.getTestUser());
+        projectService.updateProject(projectRequest);
+        verify(projectRepository, times(1)).save(any());
+        verify(userRepository, times(1)).save(any());
+    }
+
+    @Test
+    public void getAllProjectsNullUser() {
+        List<Project> expectedProjectList = Arrays.asList(TestUtil.getTestProject());
+        when(projectRepository.findAll()).thenReturn(expectedProjectList);
+        when(userRepository.findUserByProjectId(anyInt())).thenReturn(null);
+        when(taskRepository.findTasksByProjectId(anyInt())).thenReturn(Arrays.asList(TestUtil.getTestTask()));
+        List<ProjectResponse> actualProjectResponseList = projectService.getAllProjects();
+        assertEquals(actualProjectResponseList.get(0).getProjectId(), expectedProjectList.get(0).getProjectId());
+    }
+
+    @Test
+    public void getProjectNullUser() {
+        Project expectedProject = TestUtil.getTestProject();
+        when(projectRepository.findById(anyInt())).thenReturn(Optional.of(expectedProject));
+        when(userRepository.findUserByProjectId(anyInt())).thenReturn(null);
+        ProjectResponse actualProjectResponse = projectService.getProject(1);
+        assertEquals(actualProjectResponse.getProjectId(), expectedProject.getProjectId());
+    }
 }
